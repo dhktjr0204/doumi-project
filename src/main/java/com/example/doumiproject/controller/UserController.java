35840a82399f0223c845bc499d2984b8f0cae6f8
kanghanju <hanju1017@gmail.com>
2024-03-11 17:37:18 +0900

    feat: add mypage realated logic

--- src/main/java/com/example/doumiproject/controller/UserController.java
+++ src/main/java/com/example/doumiproject/controller/UserController.java
@@ -1,26 +1,24 @@
 package com.example.doumiproject.controller;
 
+import com.example.doumiproject.dto.CommentDto;
 import com.example.doumiproject.dto.PostDto;
 import com.example.doumiproject.entity.User;
 import com.example.doumiproject.service.UserService;
 
+import com.example.doumiproject.validate.UserValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
@@ -32,12 +30,11 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    //ResponseEntity는 스프링의 HTTP 응답을 나타내는 클래스다.이 클래스를 사용해 json응답을 반환한다
-    //fetch를 통해 전송된 JSON을  RequestBody를 통해 받는다
-    public ResponseEntity<?> save(@Valid @RequestBody Map<String, String> userData) {
-        //SignUpDto를 사용하지 않고 직접 User객체로 값을 넣는다
-        //UserService로 User를 회원가입시킨다
-        userService.join(userData.get("id"), userData.get("password"));
+    public ResponseEntity<?> save(@RequestBody User user, BindingResult bindingResult) {
+
+        UserValidator userValidator = new UserValidator();
+        userValidator.validate(user, bindingResult);
+        userService.join(user.getUserId(), user.getPassword());
 
         Map<String, Object> response = new HashMap<>() {{
             put("success", true);
@@ -45,18 +42,6 @@ public class UserController {
         }};
 
         return ResponseEntity.ok(response);
-//        try {
-//            userService.join(userData.get("id"), userData.get("password"));
-//            Map<String, Object> response = new HashMap<>();
-//            response.put("success", true);
-//            response.put("message", "회원가입 성공!");
-//            return ResponseEntity.ok(response);
-//        } catch (IllegalStateException e) {
-//            Map<String, Object> errorResponse = new HashMap<>();
-//            errorResponse.put("success", false);
-//            errorResponse.put("message", e.getMessage());
-//            return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
-//        }
     }
 
     @PostMapping("/user/login")
@@ -71,8 +56,6 @@ public class UserController {
         HttpSession session = request.getSession();
         session.setAttribute("userId", user.getId());
         session.setAttribute("userName", user.getUserId());
-//            session.setAttribute("loginUser",
-//                user); //session에 loginUser라는 이름의 user 객체를 담는다 (로그인 회원 정보 보관)
         session.setMaxInactiveInterval(1800);
 
         //3. 로그인 성공 응답 반환
@@ -94,19 +77,36 @@ public class UserController {
         return "mypage";
     }
 
-
-    //    수정이 필요한 코드
     @GetMapping("/user/codingtest/post")
-    @ResponseBody
-    public String userCotePost(Model model) {
-        List<PostDto> postList = new ArrayList<>();
-        PostDto testPost = new PostDto();
-        testPost.setId(1);
-        testPost.setTitle("테스트1");
-        testPost.setLikeCount(1);
-        postList.add(testPost);
+    public String getCodingTestPost(Model model, HttpSession session) {
+        long userId = (long) session.getAttribute("userId");
 
-        model.addAttribute("cote", postList);
-        return "mypage";
+        List<PostDto> userCoteList = userService.getAllUserCodingTestPosts(userId);
+
+        model.addAttribute("coteList", userCoteList);
+
+        return "mypageCodingTest";
+    }
+
+    @GetMapping("/user/quiz/post")
+    public String getQuizPost(Model model, HttpSession session) {
+        long userId = (long) session.getAttribute("userId");
+
+        List<PostDto> userQuizList = userService.getAllUserQuizPosts(userId);
+
+        model.addAttribute("quizList", userQuizList);
+
+        return "mypageQuiz";
+    }
+
+    @GetMapping("/user/comment/post")
+    public String getCommentPost(Model model, HttpSession session) {
+        long userId = (long) session.getAttribute("userId");
+
+        List<CommentDto> userCommentList = userService.getAllUserCommentPosts(userId);
+
+        model.addAttribute("commentList", userCommentList);
+
+        return "mypageComment";
     }
 }
--- src/main/java/com/example/doumiproject/repository/UserRepository.java
+++ src/main/java/com/example/doumiproject/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.doumiproject.repository;
 
+import com.example.doumiproject.dto.CommentDto;
+import com.example.doumiproject.dto.PostDto;
 import com.example.doumiproject.entity.User;
 
 import java.util.List;
+import org.springframework.jdbc.core.RowMapper;
 
 public interface UserRepository {
 
@@ -12,4 +15,21 @@ public interface UserRepository {
 
     List<User> findAllUser(); //모든 회원 리스트를 반환하는 기능
 
+    List<PostDto> findAllUserCodingTestPosts(long userId);
+
+    List<PostDto> findAllUserQuizPosts(long userId);
+
+    List<CommentDto> findAllUserCommentPosts(long userId);
+
+    default RowMapper<User> userRowMapper() {
+        return ((rs, rowNum) -> {
+            User user = User.builder()
+                .id(rs.getLong("id"))
+                .userId(rs.getString("user_id"))
+                .password(rs.getString("password"))
+                .build();
+
+            return user;
+        });
+    }
 }
