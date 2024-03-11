3619beeea9d0c031c5ec4d837cad3ce1309c6077
fernandokkang <spineattack@gmail.com>
2024-03-11 17:28:00 +0900

    refactor: modify build.gradle

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-WAR_FILE="$PROJECT_ROOT/doumi-web-app.war"
+JAR_FILE="$PROJECT_ROOT/doumi-web-app.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $WAR_FILE)
+CURRENT_PID=$(pgrep -f $JAR_FILE)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
