fa076ba78d5f6c3057b6316211fc8e0901345592
kanghanju <hanju1017@gmail.com>
2024-03-16 00:21:42 +0900

    feat: add automate API documentation use swagger

--- src/main/java/com/example/doumiproject/controller/FileController.java
+++ src/main/java/com/example/doumiproject/controller/FileController.java
@@ -2,6 +2,8 @@ package com.example.doumiproject.controller;
 
 import com.example.doumiproject.dto.FileDto;
 import com.example.doumiproject.service.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,13 @@ import java.nio.file.Files;
 
 @RestController
 @RequiredArgsConstructor
-public class FileController{
+@Tag(name = "File", description = "File Controller")
+public class FileController {
 
     private final FileService fileService;
 
     @PostMapping("/board/file")
+    @Operation(summary = "이미지 파일을 업로드하는 API???", description = "??")
     public String fileWrite(@RequestBody MultipartFile file) throws IOException {
 
         FileDto fileDto = fileService.fileWrite(file);
@@ -31,11 +35,13 @@ public class FileController{
     }
 
     @GetMapping("/image-print")
+    @Operation(summary = "이미지 파일을 화면에 출력하는 API???", description = "??")
     public byte[] printEditorImage(String fileName) {
 
-        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\quiz";
+        String path =
+            System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\quiz";
 
-        File uploadedFile = new File(path+'\\'+fileName);
+        File uploadedFile = new File(path + '\\' + fileName);
 
         if (uploadedFile.exists() == false) {
             throw new RuntimeException();
--- src/main/java/com/example/doumiproject/controller/LikeController.java
+++ src/main/java/com/example/doumiproject/controller/LikeController.java
@@ -2,6 +2,12 @@ package com.example.doumiproject.controller;
 
 import com.example.doumiproject.dto.LikesDto;
 import com.example.doumiproject.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +22,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/like")
+@Tag(name = "Like", description = "Like Controller")
 public class LikeController {
 
     private final LikeService likeService;
 
     @ResponseBody
     @GetMapping("")
-    public ResponseEntity<?> getLikeInfo(HttpServletRequest request, @RequestParam(value = "postId") long postId) {
+    @Operation(summary = "게시글의 총 좋아요 수를 반환하는 API", description = "게시글의 총 좋아요 수를 반환하는 API로 query로 게시글의 id값을 줄 수 있습니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+            content = @Content(mediaType = "application/json"
+            )),
+        @ApiResponse(responseCode = "500",
+
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(name = "서버 오류", value = "{\"error\": \"내부 서버 오류가 발생했습니다.\"}")
+
+            ))})
+    public ResponseEntity<?> getLikeInfo(HttpServletRequest request,
+        @RequestParam(value = "postId") long postId) {
 
         try {
 
@@ -37,7 +57,7 @@ public class LikeController {
             likesDto.setLikeCount(likeCount);
 
             return ResponseEntity.ok(likesDto);
-        }catch (Exception e) {
+        } catch (Exception e) {
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
         }
@@ -45,9 +65,21 @@ public class LikeController {
 
     @ResponseBody
     @GetMapping("/add")
+    @Operation(summary = "게시글에 좋아요를 추가하는 API", description = "게시글에 좋아요를 추가하는 API로 query로 게시글의 id값과 게시글의 type을 줄 수 있습니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+            content = @Content(mediaType = "application/json"
+            )),
+        @ApiResponse(responseCode = "500",
+
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(name = "서버 오류", value = "{\"error\": \"내부 서버 오류가 발생했습니다.\"}")
+
+            ))})
     public ResponseEntity<?> addLike(HttpServletRequest request,
-                                     @RequestParam(value = "postId") long postId,
-                                     @RequestParam(value = "type") String type) {
+        @RequestParam(value = "postId") long postId,
+        @RequestParam(value = "type") String type) {
 
         try {
             HttpSession session = request.getSession();
@@ -57,7 +89,7 @@ public class LikeController {
             long likeCount = likeService.getCountLike(postId);
 
             return ResponseEntity.ok(likeCount);
-        }catch (Exception e) {
+        } catch (Exception e) {
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
         }
@@ -65,9 +97,21 @@ public class LikeController {
 
     @ResponseBody
     @GetMapping("/cancel")
+    @Operation(summary = "게시글에 좋아요를 취소하는 API", description = "게시글에 좋아요를 취소하는 API로 query로 게시글의 id값과 게시글의 type을 줄 수 있습니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+            content = @Content(mediaType = "application/json"
+            )),
+        @ApiResponse(responseCode = "500",
+
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(name = "서버 오류", value = "{\"error\": \"내부 서버 오류가 발생했습니다.\"}")
+
+            ))})
     public ResponseEntity<?> cancelLike(HttpServletRequest request,
-                                        @RequestParam(value = "postId") long postId,
-                                        @RequestParam(value = "type") String type) {
+        @RequestParam(value = "postId") long postId,
+        @RequestParam(value = "type") String type) {
 
         try {
             HttpSession session = request.getSession();
@@ -77,7 +121,7 @@ public class LikeController {
             long likeCount = likeService.getCountLike(postId);
 
             return ResponseEntity.ok(likeCount);
-        }catch (Exception e) {
+        } catch (Exception e) {
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
         }
