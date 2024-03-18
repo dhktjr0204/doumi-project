6d2711de35ef2c2ce7f353e352279c6c230961c2
fernandokkang <spineattack@gmail.com>
2024-03-19 00:53:29 +0900

    code deploy

--- src/main/java/com/example/doumiproject/config/S3Config.java
+++ src/main/java/com/example/doumiproject/config/S3Config.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${secrets.AWS_ACCESS_KEY_ID}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${secrets.AWS_SECRET_ACCESS_KEY}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
