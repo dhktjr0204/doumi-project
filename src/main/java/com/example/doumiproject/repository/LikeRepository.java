c25fccf71e6d65c33498eccb1ff4bd3af83d87b9
fernandokkang <spineattack@gmail.com>
2024-03-21 11:26:00 +0900

    fix: modify like count error

--- src/main/java/com/example/doumiproject/repository/LikeRepository.java
+++ src/main/java/com/example/doumiproject/repository/LikeRepository.java
@@ -7,6 +7,6 @@ public interface LikeRepository {
 
     public void addLike(long user_id, long post_id, String type);
     public void cancelLike(long user_id, long post_id, String type);
-    public long countLike(long post_id);
+    public long countLike(long post_id, String type);
     public boolean existsByUserIdAndPostId(long user_id, long post_id);
 }
